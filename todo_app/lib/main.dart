a6b475418007da34439cec51a7957d5cbe677518
Barbara Ilardo <ilardobarbarasofia@gmail.com>
2024-08-07 16:10:59 +0200

    First commit

---
+++ todo_app/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:todo_app/login_screen.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp;
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "ToDo",
+      theme:
+          ThemeData(primarySwatch: Colors.indigo, primaryColor: Colors.indigo),
+      home: const LoginScreen(),
+    );
+  }
+}
---
+++ todo_app/lib/services/auth_services.dart
@@ -0,0 +1,19 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // Sign in
+
+  Future<User?> _signInWithPassword(String email, String password) async {
+    try {
+      UserCredential result = await _auth.signInWithEmailAndPassword(
+          email: email, password: password);
+      User? user = result.user;
+      return user;
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+}
